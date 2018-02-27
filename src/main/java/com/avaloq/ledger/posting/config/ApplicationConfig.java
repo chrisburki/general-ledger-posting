7cf73b7a8aee84a6ef00f3759c28a82a23bb5ae9
Christoph Burki <chrisburki@gmx.ch>
2018-02-27 05:49:11 +0100

    Add Swagger apiInfo

--- src/main/java/com/avaloq/ledger/posting/config/ApplicationConfig.java
+++ src/main/java/com/avaloq/ledger/posting/config/ApplicationConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -20,6 +24,19 @@ public class ApplicationConfig {
         .paths(PathSelectors.any())
         .build();
   }
+
+  private ApiInfo apiInfo() {
+    ApiInfo apiInfo = new ApiInfo(
+        "General Ledger Posting Rule REST API",
+        "General Ledger Posting Rule description of API.",
+        "1.0",
+        "Terms of service",
+        new Contact("Christoph Burki", "www.avaloq.com", "christoph.burki@avaloq.com"), "License of API", "API license URL", Collections.emptyList()
+    );
+    return apiInfo;
+  }
+
+
 }
 
 //http://localhost:8080/swagger-ui.html
